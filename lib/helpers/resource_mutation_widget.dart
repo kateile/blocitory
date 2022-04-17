b85fd1733b925ee5f9f9cfbdaced98b12d88cba8
Sylvanus Kateile <kateile@outlook.com>
2022-04-17 09:26:18 +0300

    add bloc provider in mutation builder

--- lib/helpers/resource_mutation_widget.dart
+++ lib/helpers/resource_mutation_widget.dart
@@ -4,6 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/widgets.dart';
 import 'resource_state.dart';
 
+typedef ItemCreator<B, R> = B Function(R repository);
+
 typedef ResourceMutationBuilder<B> = Widget Function(
     BuildContext context, B cubit);
 
@@ -13,10 +15,11 @@ typedef ResourceMutationOnSuccess<D> = void Function(
 typedef ResourceMutationOnError = void Function(
     BuildContext context, String message);
 
-class MutationBuilder<T, B extends Cubit<ResourceState<T>>>
+class MutationBuilder<T, B extends Cubit<ResourceState<T>>, R>
     extends StatelessWidget {
   /// This will build a widget that will be shown before mutation like form etc.
   final ResourceMutationBuilder<B> builder;
+  final ItemCreator<B, R> blocCreator;
 
   /// If error occurred during mutation
   final ResourceMutationOnError? onError;
@@ -38,6 +41,7 @@ class MutationBuilder<T, B extends Cubit<ResourceState<T>>>
   const MutationBuilder({
     Key? key,
     required this.builder,
+    required this.blocCreator,
     this.onError,
     this.onSuccess,
     this.successMessage,
@@ -48,70 +52,77 @@ class MutationBuilder<T, B extends Cubit<ResourceState<T>>>
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<B, ResourceState>(
-      listener: (context, state) {
-        //Here onSuccess can have side effects like navigation
-        if (state.status == ResourceStatus.success && !showSuccessBody) {
-          if (onSuccess != null && state.data != null) {
-            onSuccess!(context, state.data);
-          } else {
-            displaySuccess(
-              context: context,
-              message: successMessage ?? 'Success',
-            );
-
-            if (pop) Navigator.of(context).pop();
-          }
-        }
-
-        if (state.status == ResourceStatus.error && state.message != null) {
-          //If onError is null we use snack bar to show error.
-          if (onError != null) {
-            onError!(context, state.message!);
-          } else {
-            displayError(
-              context: context,
-              message: state.message!,
-            );
-
-            if (pop) Navigator.of(context).pop();
-          }
-        }
+    return BlocProvider<B>(
+      create: (context) {
+        return blocCreator(
+          RepositoryProvider.of<R>(context),
+        );
       },
-      builder: (context, state) {
-        final cubit = BlocProvider.of<B>(context);
+      child: BlocConsumer<B, ResourceState>(
+        listener: (context, state) {
+          //Here onSuccess can have side effects like navigation
+          if (state.status == ResourceStatus.success && !showSuccessBody) {
+            if (onSuccess != null && state.data != null) {
+              onSuccess!(context, state.data);
+            } else {
+              displaySuccess(
+                context: context,
+                message: successMessage ?? 'Success',
+              );
 
-        if (state.status == ResourceStatus.loading) {
-          return loadingWidget ?? const LoadingIndicator();
-        }
+              if (pop) Navigator.of(context).pop();
+            }
+          }
 
-        //todo should we also use alert here
-        if (showSuccessBody && state.status == ResourceStatus.success) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                successMessage ?? 'Action Completed',
-                maxLines: 2,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+          if (state.status == ResourceStatus.error && state.message != null) {
+            //If onError is null we use snack bar to show error.
+            if (onError != null) {
+              onError!(context, state.message!);
+            } else {
+              displayError(
+                context: context,
+                message: state.message!,
+              );
+
+              if (pop) Navigator.of(context).pop();
+            }
+          }
+        },
+        builder: (context, state) {
+          final cubit = BlocProvider.of<B>(context);
+
+          if (state.status == ResourceStatus.loading) {
+            return loadingWidget ?? const LoadingIndicator();
+          }
+
+          //todo should we also use alert here
+          if (showSuccessBody && state.status == ResourceStatus.success) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  successMessage ?? 'Action Completed',
+                  maxLines: 2,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              const Divider(),
-              Button(
-                callback: () {
-                  Navigator.of(context).pop();
-                },
-                title: 'Go Back',
-              )
-            ],
-          );
-        }
+                const Divider(),
+                Button(
+                  callback: () {
+                    Navigator.of(context).pop();
+                  },
+                  title: 'Go Back',
+                )
+              ],
+            );
+          }
 
-        //We always show form when not loading.
-        return builder(context, cubit);
-      },
+          //We always show form when not loading.
+          return builder(context, cubit);
+        },
+      ),
     );
   }
 }
