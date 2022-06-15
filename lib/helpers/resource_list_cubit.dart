e3eebffc8484ca811639cab5cfbab368d0ff362b
Sylvanus Kateile <kateile@outlook.com>
2022-06-15 10:36:23 +0300

    add update item in list functionality

--- lib/helpers/resource_list_cubit.dart
+++ lib/helpers/resource_list_cubit.dart
@@ -13,4 +13,20 @@ class ResourceListCubit<T> extends ResourceCubit<ResourceListData<T>> {
       addedItems: [item],
     ));
   }
+
+  updateItem(T Function(List<T> e) test, T newItem) {
+    final list = state.data?.items;
+
+    try {
+      final old = test(list ?? <T>[]);
+      list?.remove(old);
+    } catch (_) {}
+
+    list?.insert(0, newItem);
+
+    super.update(state.data?.copyWith(
+      items: list,
+      updatedItems: [newItem],
+    ));
+  }
 }
